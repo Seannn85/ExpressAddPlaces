b875433a33afddb7d91b2e2735e615649fbc6da1
Goktug Koyun <koyun.gok2@gmail.com>
2022-09-10 00:16:10 +0300

    add error handling

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
+const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.listen(5000);
+app.use('/api/places', placesRoutes); // => /api/places ...
+
+app.use((error,req, res, next)=> {
+
+    if(res.headerSent){
+        return next(error);
+    }
+
+    res.status(error.code || 500);
+    res.json({message: error.message || 'An known error occured!'});
+});
+app.listen(3300);
